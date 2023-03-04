cba3d1d1b3ee8298c442faff0c169c0d3e444409
FredRost <fredrost161@gmail.com>
2023-03-04 14:00:05 +0300

    Done Task14

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool checkNumber = CheckNumber(number);
+bool CheckNumber(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
+Console.WriteLine(checkNumber ? "Да" : "Нет");
