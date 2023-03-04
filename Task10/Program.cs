46a66d8a17ec9db2a833045fde9d25d07a0035ae
FredRost <fredrost161@gmail.com>
2023-03-04 13:36:15 +0300

    Done Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(number);
+Console.WriteLine("Вторая цифра числа: " + stringNumber[1]);
