b1907e26985e092bc5a69b88cd2d2f39763891e7
FredRost <fredrost161@gmail.com>
2023-03-05 21:53:31 +0300

    Done Task18

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите номер четверти: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string coord = Coord(number);
+Console.WriteLine(coord);
+
+string Coord(int num)
+{
+    if(num == 1) return "x > 0 и y > 0";
+    if(num == 2) return "x < 0 и y > 0";
+    if(num == 3) return "x < 0 и y < 0";
+    if(num == 4) return "x > 0 и y < 0";
+    return "Введена некорректная четверть";
+}
+
