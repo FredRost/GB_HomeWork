d3131f64d4db8bd8edad9e49c4478f98dca72572
FredRost <fredrost161@gmail.com>
2023-03-06 21:03:11 +0300

    Done Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int numberOfDigits = NumberOfDigits(number);
+Console.WriteLine($"Количество цифр в числе {number} = {numberOfDigits}");
+
+
+int NumberOfDigits (int num)
+{
+    int i = 0;
+    while (num > 0)
+    {
+        num = num / 10;
+        i ++;
+    }
+    return i;
+}
