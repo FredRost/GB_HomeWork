5cc6facb5ce0bc528002d59402d82a8c149f27f0
FredRost <fredrost161@gmail.com>
2023-03-06 22:08:31 +0300

    Done Task30

---
+++ Task30/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] array = new int [8];
+Random rand = new Random();
+
+Array(array);
+WriteArray(array);
+
+void Array(int[] arr)
+{
+Random rand = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rand.Next(0, 2);
+}
+}
+
+void WriteArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i]);
+    }
+}
