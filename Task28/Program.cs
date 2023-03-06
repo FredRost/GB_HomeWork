89d487c9b7bb329ca738d22d1df39845b995d96a
FredRost <fredrost161@gmail.com>
2023-03-06 21:18:23 +0300

    Done Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int multiplication = Multiplication(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {multiplication}");
+
+int Multiplication (int num)
+{
+    int mult = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        mult = mult * i;
+    }
+    return mult;
+}
